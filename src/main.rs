f0ea7e184e80df4d5c31aaceb5a1f0902561f739
Jason Nwaeze <110257587+Jacey77nDIL@users.noreply.github.com>
2025-01-25 13:54:32 +0100

    Update main.rs CORS origin

--- src/main.rs
+++ src/main.rs
@@ -322,7 +322,7 @@ async fn main() {
         .route("/get_battery_capacity_for_laptops", post(get_battery_capacity_for_laptops))
         .layer(
             CorsLayer::new()
-                .allow_origin(Any) // Allow requests from any origin
+                .allow_origin("https://verdant-melba-b66cfd.netlify.app") // Allow requests from my site
                 .allow_methods(Any) // Allow any HTTP method
                 .allow_headers(Any), // Allow any headers
         );
